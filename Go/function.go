45ba621b639be29c6fd1867c7ea4f7b676fcaedb
PlaneVector <1957570662@qq.com>
2019-08-13 11:31:11 +0800

    exaple function

--- Go/function.go
+++ Go/function.go
@@ -1,11 +1,26 @@
 package main
 
 import "fmt"
-
+//example one
 func add(x int ,y int) int{
 	return x+y
 }
 
+//example two
+
+func a_dd(x ,y int)int{
+	return x+y
+}
+
+//example three returns multiple values
+
+func swap(x,y string) (string,string){
+	return y,x
+}
+
 func main(){
-fmt.Println(add(12,21))
+fmt.Println("exaple one",add(12,21))
+fmt.Println("exaple two",a_dd(12,33))
+a,b :=swap("ok","are you")
+fmt.Println("exaple three",a,b)
 }
