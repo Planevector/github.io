7243d3e2b5535e225a823c27f48fa6fe77d73ad3
PlaneVector <1957570662@qq.com>
2019-08-15 11:42:10 +0800

    switch example

--- Go/switch_sentence.go
+++ Go/switch_sentence.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main(){
-	s :="a"
+	{
+		s :="a"
 	switch s{
 	case "a":
 	fmt.Println("yes")
 	case "b":
 	fmt.Println("no")
 	}
+	}
+	{
+t :=time.Now()
+switch{
+case t.Hour()<12:
+	fmt.Println("good monrning")
+case t.Hour()<17:
+	fmt.Println("good afternoon")
+default:fmt.Println("good evening")
+}
+}
 }
